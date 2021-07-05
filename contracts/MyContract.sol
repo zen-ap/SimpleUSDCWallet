04751d066c406b74f08811400ada873d94cb1321
zen-ap <78710266+zen-ap@users.noreply.github.com>
2021-07-05 02:11:15 -0400

    test

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,6 +2,11 @@
 
 pragma solidity 0.6.11;
 
+
 contract MyContract {
     constructor() public {}
+
+    const wallet = ethers.Wallet.createRandom();
+    console.log(`Mnemonic: ${wallet.mnemonic.phrase}`);
+    console.log(`Address: ${wallet.address}`);
 }
